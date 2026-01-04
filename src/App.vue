5ac8459ed6c44c7fbb1213af8a27297a32771bc3
Mohit Chandana <chandanamohit@gmail.com>
2026-01-04 13:18:54 -0500

    Create header for nav buttons

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <router-view />
+  <div>
+    <Header v-if="isAuthenticated" />
+    <router-view />
+  </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { useUserStore } from '@/store/user';
+import Header from "@/components/Header.vue";
+
+const userStore = useUserStore();
+const isAuthenticated = computed(() => !!userStore.jwt);
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-white shadow p-4 flex justify-between items-center">
+    <h1 class="text-xl font-bold">SMS Messenger</h1>
+
+    <div class="space-x-4">
+      <router-link to="/messages"
+        class="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600">Messages</router-link>
+      <router-link to="/send" class="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 ml-4">Send
+        Message</router-link>
+      <button @click="logout" class="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600">Logout</button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { useUserStore } from "@/store/user";
+
+const router = useRouter();
+const userStore = useUserStore();
+
+const logout = () => {
+  userStore.logout();
+  router.push("/login");
+};
+</script>
