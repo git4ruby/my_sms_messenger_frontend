15241c52efc8c42a8fa7a516c5d519bf47910923
Mohit Chandana <chandanamohit@gmail.com>
2026-01-05 14:53:54 -0500

    Add API Base URL of backend prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
@@ -16,3 +17,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+``
